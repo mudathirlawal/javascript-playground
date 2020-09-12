f354e0d92b9a96f7de9f02ef8fc0ceefb5068061
Mudathir Lawal <nigercode@gmail.com>
2020-09-12 15:54:38 +0100

    Create interestingClosure.js
    This is an interestiong implementation of closure.

---
+++ interestingClosure.js
@@ -0,0 +1,13 @@
+function myStr1(start, step) {
+    return function() {
+        const curr = start;
+        start += step;
+        return curr;
+    }
+}
+
+const myStr2 = myStr1(5, 2);
+myStr2();
+myStr2();
+myStr2();
+console.log( myStr2() );
