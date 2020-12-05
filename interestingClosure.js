96c0c0e820b1b693175f1d1f2ebf2d971be028c3
Mudathir Lawal <nigercode@gmail.com>
2020-12-05 15:53:51 +0100

    doc: Add questions in comments section

--- interestingClosure.js
+++ interestingClosure.js
@@ -1,6 +1,5 @@
 // What does the console log?
-
-function myStr1(start, step) {
+function myStr1( start, step ) {
     return function() {
         const curr = start;
         start += step;
@@ -8,8 +7,16 @@ function myStr1(start, step) {
     }
 }
 
-const myStr2 = myStr1(5, 2);
+const myStr2 = myStr1( 5, 2 );
 myStr2();
 myStr2();
 myStr2();
 console.log( myStr2() );
+
+// What is the correct way to call this function?
+// A: const sum4 = sum( 4 ); sum4( 5 );
+// B: sum( 5 )( 4 );
+// C: sum( 5, 4 );
+function sum( x ) {
+    return y => x + y;
+}
